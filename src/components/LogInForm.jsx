6a1480d88770965b496352009a1998b3934c6a0e
Nathan Jones <46408104+Glitch-v0@users.noreply.github.com>
2024-11-26 10:43:25 -0600

    Add auto-navigation away from login page upon successful login

--- src/components/LogInForm.jsx
+++ src/components/LogInForm.jsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
+import { logIn } from "../utils/authenticationMethods";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
   return (
     <form
       action={`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/login`}
       id="loginForm"
       method="post"
+      onSubmit={(e) => {
+        e.preventDefault();
+        logIn(email, password, navigate);
+      }}
     >
       <h2>Log In</h2>
       <div>
         <label>
           <h3>Email</h3>
-          <input type="text" name="email" placeholder="email@address.com" />
+          <input
+            type="text"
+            name="email"
+            placeholder="email@address.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label>
           <h3>Password</h3>
-          <input type="text" name="password" placeholder="password" />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
       </div>
       <button type="submit">Submit</button>
--- src/utils/authenticationMethods.js
+++ src/utils/authenticationMethods.js
@@ -5,3 +5,24 @@ export function saveToken(token) {
 export function getToken() {
   return localStorage.getItem("token");
 }
+
+export async function logIn(email, password, navigate) {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password: password }),
+      }
+    );
+    const data = await response.json();
+    saveToken(data.token);
+    navigate("/blog");
+    return console.log(data);
+  } catch (error) {
+    return console.error(error);
+  }
+}
