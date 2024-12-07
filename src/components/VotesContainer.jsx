730d995541b76cfe0d1f0ad160d37e6027c9e9f7
Nathan Jones <46408104+Glitch-v0@users.noreply.github.com>
2024-12-07 13:17:36 -0600

    comment vote totals now update upon voting

--- src/components/VotesContainer.jsx
+++ src/components/VotesContainer.jsx
@@ -1,22 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import upvoteIcon from "../assets/bxs--upvote.svg";
 import downvoteIcon from "../assets/bxs--downvote.svg";
 import propTypes from "prop-types";
 
 export default function VotesContainer({ votes, userVote }) {
+  const [totalVotes, setTotalVotes] = useState(null);
   const [vote, setVote] = useState(userVote);
 
-  let totalVotes = 0;
-  for (let i = 0; i < votes.length; i++) {
-    totalVotes += votes[i].value;
-  }
   VotesContainer.propTypes = {
     votes: propTypes.array,
     userVote: propTypes.bool,
   };
+
+  // Calculate total votes
+  useEffect(() => {
+    const initialTotal = votes.reduce((sum, vote) => sum + vote.value, 0);
+    setTotalVotes(initialTotal);
+  }, [votes]);
+
+  const handleVote = (upOrDown) => {
+    if (vote === upOrDown) {
+      // Undo current vote
+      setVote(null);
+      upOrDown ? setTotalVotes(totalVotes - 1) : setTotalVotes(totalVotes + 1);
+    } else if (vote == null) {
+      // New vote (upvote or downvote)
+      setVote(upOrDown);
+      upOrDown ? setTotalVotes(totalVotes + 1) : setTotalVotes(totalVotes - 1);
+    } else {
+      // Change from upvote to downvote or vice versa
+      setVote(upOrDown);
+      upOrDown ? setTotalVotes(totalVotes + 2) : setTotalVotes(totalVotes - 2);
+    }
+  };
+
   return (
     <div className="votesContainer">
-      <button>
+      <button className="upvoteButton" onClick={() => handleVote(true)}>
         <img
           src={upvoteIcon}
           alt="upvote icon"
@@ -25,7 +45,7 @@ export default function VotesContainer({ votes, userVote }) {
         />
       </button>
       <p>{totalVotes}</p>
-      <button>
+      <button className="downvoteButton" onClick={() => handleVote(false)}>
         <img
           src={downvoteIcon}
           alt="downvote icon"
